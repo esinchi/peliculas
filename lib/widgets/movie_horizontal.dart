c1a214f32047638f21886ece127946154f238791
WASHINGTON SINCHIGUANO <esinchi@hotmail.com>
2021-05-13 16:48:18 -0500

    uno mas

--- lib/widgets/movie_horizontal.dart
+++ lib/widgets/movie_horizontal.dart
@@ -37,7 +37,7 @@ class MovieHorizontal extends StatelessWidget {
   }
 
   Widget _tarjeta(BuildContext context, Pelicula pelicula) {
-    return Container(
+    final tarjetaPelicula = Container(
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: [
@@ -58,6 +58,13 @@ class MovieHorizontal extends StatelessWidget {
               style: Theme.of(context).textTheme.caption)
         ],
       ),
+    );
+
+    return GestureDetector(
+      child: tarjetaPelicula,
+      onTap: (){
+        print(pelicula.title);
+      },
     );
   }
 
